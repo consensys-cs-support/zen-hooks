90ac573bc05cd31ae3ba6676f7b3089dd4939f6a
Itamar Levy-Or <ilevyor@umass.edu>
2018-08-17 10:32:24 -0400

    first commit

---
+++ routes.js
@@ -0,0 +1,6 @@
+module.exports = function(app){
+
+    app.get('/', function(req, res) {
+        res.json({ message: 'hooray! welcome to our api!' });   
+    });
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+require('./routes')(app);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
